ee952a77d697df2f285c85ae2c66a7e4fda32ebc
Matheus Ferreira <matheusfernandofreire@hotmail.com>
2020-06-04 17:24:36 -0300

    Começo da API

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,9 +2,28 @@ import express from 'express';
 
 const app = express();
 
+const users = ['Matheus', 'Dailon', 'Carlos'];
 app.get('/users', (request, response) => {
-    console.log('Listagem de Usuários');
-    response.json(['Matheus', 'Dailon', 'Carlos']);
+    const search = request.query.search;
+
+    return response.json(users);
+});
+
+app.post('/users', (request, response) => {
+    const user = {
+        nome: 'Matheus',
+        email: 'matheus.franca@alterdata.com.br'
+    };
+
+    return response.json(user);
+});
+
+app.get('/users/:id', (request, response) => {
+    const id = Number(request.params.id);
+
+    const user = users[id];
+
+    return response.json(user);
 });
 
 app.listen(3333);
