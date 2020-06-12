32048eac35be4ec7f99af36a063f958bb0999972
Matheus Ferreira <matheusfernandofreire@hotmail.com>
2020-06-12 17:50:09 -0300

    App Mobile Quase Finalizado

---
+++ mobile/src/Point.ts
@@ -0,0 +1,15 @@
+export interface ItemTitle {
+    title: string
+};
+
+export interface Point {
+    id: number,
+    name: string,
+    email: string,
+    image: string,
+    whatsapp: string,
+    latitude: number,
+    longitude: number,
+    city: string,
+    uf: string
+};
